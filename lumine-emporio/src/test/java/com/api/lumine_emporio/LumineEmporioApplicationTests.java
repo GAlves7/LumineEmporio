0329d234c7d4fe539f4899aead099c62ee1bf0b8
Juan-82 <juanenoque82@gmail.com>
2025-10-20 20:35:36 -0300

    inicio projeto
    inicio do projeto

---
+++ lumine-emporio/src/test/java/com/api/lumine_emporio/LumineEmporioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.lumine_emporio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumineEmporioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
