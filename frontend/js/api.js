5de67ca5d446e7517aa674f9b84f44969fc1813b
CarlosEduHh <eduardosilva17105@gmail.com>
2025-11-23 11:01:41 -0300

    Delete frontend directory

--- frontend/js/api.js
+++
@@ -1,9 +0,0 @@
-// api.js
-
-// Cria uma instância do Axios com a URL base do backend
-const api = axios.create({
-    baseURL: "http://localhost:8080", // 🟡 Altere para o endereço do seu backend Spring Boot
-});
-
-// Exporta para ser usada em outros scripts
-export default api;
--- frontend/js/script.js
+++
@@ -1,5 +0,0 @@
-// Importa o script responsável pelo controle do header (busca expandida, tema, ícones, etc.)
-import './header.js';
-
-// Importa o script responsável pelo modal de login e funcionalidades relacionadas (mostrar/ocultar senha, abrir/fechar modal)
-import './login.js';
