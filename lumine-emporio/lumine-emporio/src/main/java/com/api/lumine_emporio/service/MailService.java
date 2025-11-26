a22c6794cb36d497696cd7feb7df7c3625c0aefc
Niel2D <daniel.antoniosilvape@gmail.com>
2025-11-26 19:42:17 -0300

    atualizacao no email
    agora o email usa api, o resend

--- lumine-emporio/lumine-emporio/src/main/java/com/api/lumine_emporio/service/MailService.java
+++ lumine-emporio/lumine-emporio/src/main/java/com/api/lumine_emporio/service/MailService.java
@@ -2,33 +2,35 @@ package com.api.lumine_emporio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import com.resend.*;
 
 @Service
 public class MailService {
-	@Autowired
-	private JavaMailSender javaMailSender;
-	
 	@Value("${spring.mail.username}")
 	private String remetente;
-	
-	@Async
-	public void enviarEmailTexto(String destinatario, String assunto, String mensagem) {
-		try {
-			SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-			simpleMailMessage.setFrom(remetente);
-			simpleMailMessage.setTo(destinatario);
-			simpleMailMessage.setSubject(assunto);
-			simpleMailMessage.setText(mensagem);
-			
-			javaMailSender.send(simpleMailMessage);
-			System.out.println("Email enviado com sucesso."); ;
-		}catch(Exception e) {
-			System.out.println("Erro ao enviar email: "+e.getMessage());
-		}
+
+	private final Resend resend;
+
+	public MailService(){
+		this.resend = Resend("re_fp2C6EAD_KuQNkr9UtUQY5ALWujDvRUE4");
 	}
 
+
+	@Async
+	public void enviarEmailTexto(String destinatario, String assunto, String mensagem) {
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("Acme <onboarding@resend.dev>")
+                .to(destinatario)
+                .subject(assunto)
+                .html("<strong>mensagem</strong>")
+                .build();
+         try {
+            CreateEmailResponse data = resend.emails().send(params);
+            System.out.println(data.getId());
+        } catch (ResendException e) {
+            e.printStackTrace();
+        }
+    }
 }
