d7a69c79f2100d5e1e7809eca57c0304f4325919
Juan-82 <juanenoque82@gmail.com>
2025-11-28 12:20:46 -0300

    apresentacao back main

--- backend/lumine-emporio/src/main/java/com/api/lumine_emporio/service/MailService.java
+++ backend/lumine-emporio/src/main/java/com/api/lumine_emporio/service/MailService.java
@@ -1,41 +1,34 @@
 package com.api.lumine_emporio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import com.resend.Resend;
-import com.resend.core.exception.ResendException;
-import com.resend.services.emails.model.CreateEmailOptions;
-import com.resend.services.emails.model.CreateEmailResponse;
-
-
 @Service
 public class MailService {
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Value("${spring.mail.username}")
 	private String remetente;
-
-	private final Resend resend;
-
-	public MailService(){
-		this.resend = new Resend("re_SPd1dAFv_PfGkp7cpQr5gp2zqEskAvfW5");
-	}
-
-
+	
 	@Async
 	public void enviarEmailTexto(String destinatario, String assunto, String mensagem) {
-        CreateEmailOptions params = CreateEmailOptions.builder()
-                .from("notificacao@lumineemporio.store")
-                .to(destinatario)
-                .subject(assunto)
-                .html(mensagem)
-                .build();
-         try {
-            CreateEmailResponse data = resend.emails().send(params);
-            System.out.println(data.getId());
-        } catch (ResendException e) {
-            e.printStackTrace();
-            System.out.println("Falha ao enviar email: "+e.getMessage());
-        }
-    }
+		try {
+			SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+			simpleMailMessage.setFrom(remetente);
+			simpleMailMessage.setTo(destinatario);
+			simpleMailMessage.setSubject(assunto);
+			simpleMailMessage.setText(mensagem);
+			
+			javaMailSender.send(simpleMailMessage);
+			System.out.println("Email enviado com sucesso."); ;
+		}catch(Exception e) {
+			System.out.println("Erro ao enviar email: "+e.getMessage());
+		}
+	}
+
 }
